4479973a766b728dc2823b4968439dcad25849e0
Martin Styk <martin.styk@gmail.com>
2017-07-27 13:44:16 +0200

    #65 Clean navigation drawer  Close #65

--- app/src/main/java/sk/piskula/fuelup/screens/MainActivity.java
+++ app/src/main/java/sk/piskula/fuelup/screens/MainActivity.java
@@ -158,20 +158,11 @@ public class MainActivity extends AppCompatActivity implements OnNavigationItemS
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.vehicle_list) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.about) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
